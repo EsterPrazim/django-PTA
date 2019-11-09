1d9c958b36db24c76e6dd81b2749a631818ef1f4
Ester Prazim <eap4@cin.ufpe.br>
2019-11-09 02:05:07 -0300

    First model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Servico
 
 # Register your models here.
+
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date')
+
+admin.site.register(Servico, ServicoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Servico(models.Model):
+    title = models.CharField('Título', max_length=50)
+    text = models.TextField('Texto', null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Data de Criação')
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return self.title
