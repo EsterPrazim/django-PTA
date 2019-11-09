1a436af71dac64539bde0637f8b25dd9ac3c4e54
Ester Prazim <eap4@cin.ufpe.br>
2019-11-09 01:38:41 -0300

    Add template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+class HomeView(generic.TemplateView):
+    template_name = 'home.html'
--- djangoPTA/urls.py
+++ djangoPTA/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name = 'base'),
 ]
