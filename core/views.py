d5a7c9c21a8be2ea3ad93ba749a0df821e4064f6
Ester Prazim <eap4@cin.ufpe.br>
2019-11-09 02:29:45 -0300

    Add services in template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import Servico, Depoimento
+
 # Create your views here.
 class HomeView(generic.TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["servicos"] = Servico.objects.all()
+        context["depoimentos"] = Depoimento.objects.all()
+        return context
