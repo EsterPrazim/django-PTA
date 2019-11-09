a7180c0c4aea2cf0a5ed5485fdad7fba90399fc8
Ester Prazim <eap4@cin.ufpe.br>
2019-11-09 02:33:36 -0300

    Add model image in template

--- djangoPTA/urls.py
+++ djangoPTA/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from core.views import HomeView
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name = 'base'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
